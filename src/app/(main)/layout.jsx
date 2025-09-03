b9fdc86bc1225554e1019cfe189b37a9268642b2
Muhammad Habeel <habeelakram80@gmail.com>
2025-09-03 14:31:28 +0500

    RegisterFunctional

--- src/app/(main)/layout.jsx
+++ src/app/(main)/layout.jsx
@@ -2,7 +2,13 @@
 
 import Navbar from '../components/navbar/page'
 import Footer from '../components/footer/page'
-import '../globals.css' // Assuming you have a global CSS file
+import '../globals.css'
+import { AuthProvider } from '../context/authContext'
+
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+// Create a client instance outside the component
+const queryClient = new QueryClient();
 
 export default function Layout({ children }) {
   return (
@@ -10,9 +16,11 @@ export default function Layout({ children }) {
       <body>
         <div className="font-sans bg-zinc-100 min-h-screen flex flex-col">
           <Navbar />
-          <main className="flex-grow">
-            {children}
-          </main>
+          <AuthProvider>
+            <QueryClientProvider client={queryClient}>
+              {children}
+            </QueryClientProvider>
+          </AuthProvider>
           <Footer />
         </div>
       </body>
